61c44b094da01fb5227cadf1ed3a1bfd266eb944
Dr. J. K. Verma <jk-verma@ieee.org>
2026-04-09 13:18:40 +0530

    Show all homepage news items from data file

--- js/script.js
+++ js/script.js
@@ -201,7 +201,7 @@ async function loadNews() {
     renderNewsTicker(items);
 
     if (container) {
-      container.replaceChildren(...items.slice(0, 2).map(renderNewsItem));
+      container.replaceChildren(...items.map(renderNewsItem));
     }
   } catch (error) {
     if (ticker) {
